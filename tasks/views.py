3131a3711b509285b9643846ba5f565cf83310f3
Oleksii Drenov <leshadrenev@gmail.com>
2025-08-22 17:22:10 +0300

    feat: add index view and urls for tasks app

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from tasks.models import Worker, Task, TaskType, Position
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_workers = Worker.objects.count()
+    num_tasks = Task.objects.count()
+    num_task_types = TaskType.objects.count()
+    num_positions = Position.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_workers": num_workers,
+        "num_tasks": num_tasks,
+        "num_task_types": num_task_types,
+        "num_positions": num_positions,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "tasks/index.html", context=context)
