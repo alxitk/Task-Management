9a91b493f736b6fad36b928d3294dce79bc69996
Oleksii Drenov <leshadrenev@gmail.com>
2025-08-22 17:12:09 +0300

    refactor: update __str__ method for Worker model

--- tasks/models.py
+++ tasks/models.py
@@ -23,7 +23,7 @@ class Worker(AbstractUser):
         ordering = ("username",)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.username})"
+        return f"{self.first_name} {self.last_name} ({self.position})"
 
 
 class TaskType(models.Model):
