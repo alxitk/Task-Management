9a5a4d5c45136ec93551b474a2e735d09f29e245
Oleksii Drenov <leshadrenev@gmail.com>
2025-08-23 00:58:47 +0300

    feat: register url path for TasksListView

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from tasks.views import index
+from tasks.views import index, TasksListView
 
 app_name = "tasks"
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "tasks/",
+        TasksListView.as_view(),
+        name="task-list",
+    ),
     ]
